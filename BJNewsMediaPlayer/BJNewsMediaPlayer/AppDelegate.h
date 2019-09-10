451743b9832b9d93845dbf194af647501f03afb0
Jacobs12 <1440913385@qq.com>
2019-09-10 09:42:41 +0800

    commit

---
+++ BJNewsMediaPlayer/BJNewsMediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BJNewsMediaPlayer
+//
+//  Created by wolffy on 2019/9/10.
+//  Copyright © 2019 新京报社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BJNewsMediaPlayer/BJNewsMediaPlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BJNewsMediaPlayer
+//
+//  Created by wolffy on 2019/9/10.
+//  Copyright © 2019 新京报社. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
